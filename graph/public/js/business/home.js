142cfeb5a672be8d64e575ca26bf668cad3ef6f2
wangkun38 <wangkun12@jd.com>
2016-04-13 11:02:08 +0800

    home

--- graph/public/js/business/home.js
+++ graph/public/js/business/home.js
@@ -4,13 +4,31 @@
 
 window.onload=function(){
     changeDivHeight();
+    changeBtnBottom();
 }
 //当浏览器窗口大小改变时，设置显示内容的高度
 window.onresize=function(){
     changeDivHeight();
+    changeBtnBottom();
 }
+
+
+//板块1高度自适应
 function changeDivHeight(){
     var width = window.innerWidth;
     var showHeight = width/1366*500;
     $('.home-top').css('height' , showHeight+'px');
+}
+//start位置
+function changeBtnBottom(){
+    var height = $('.home-top').height();
+    var showBottom = (height - 450)/3*2;
+    $('.btn-diy').css('bottom' , showBottom+'px');
+}
+
+//home-exp宽度
+function changeExpWidth(){
+    var height = $('.home-top').height();
+    var showBottom = (height - 450)/3*2;
+    $('.btn-diy').css('bottom' , showBottom+'px');
 }
